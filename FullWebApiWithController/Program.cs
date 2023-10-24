02d98cd46822a2a5f54fc0f145f7b697268cb61d
balamurugan subramaniam <balamurugan.subramaniam@leicabiosystems.com>
2023-10-24 13:03:48 +1100

    Removed binaries

--- FullWebApiWithController/Program.cs
+++ FullWebApiWithController/Program.cs
@@ -1,6 +1,16 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Logging
+    .ClearProviders()
+    .AddSerilog(new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .Enrich.WithThreadId()
+        .CreateLogger())
+    .AddConsole();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +18,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.Services.GetRequiredService<ILogger<Program>>().LogInformation("Starting Web Api...");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
