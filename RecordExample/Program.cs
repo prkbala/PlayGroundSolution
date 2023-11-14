67e2e0b19a94710f3f9258f3b2f7adc435b638be
balamurugan subramaniam <balamurugan.subramaniam@leicabiosystems.com>
2023-11-14 15:13:06 +1100

    Updated the Record example

--- RecordExample/Program.cs
+++ RecordExample/Program.cs
@@ -1,20 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// When to use records
+
+// Consider using a record in place of a class or struct in the following scenarios:
+    // - You want to define a data model that depends on value equality.
+    // - You want to define a type for which objects are immutable.
+
+var bala = new Person ("Bala", new DateOnly(1985, 8, 13));
+
+// Prints the parameters by default
+Console.WriteLine(bala);
+
+var arjun = bala with { Name = "Arjun"};
+Console.WriteLine(arjun);
 
 
-AppDetails appDetails = new("SomeApp", "2.1.0");
-Console.WriteLine(TransformStringUsingWith(appDetails));
+var anotherInstanceBala = new Person("Bala", new DateOnly(1985, 8, 13));
+// Returns True. Equality checks for the value of the params and not the reference, unlike in class
+Console.WriteLine(bala.Equals(anotherInstanceBala));
 
-static string TransformStringUsingWith(AppDetails appDetails)
-    => appDetails switch
-    {
-        { Name: "SomeApp" } => (appDetails with { Name = "SomeName_With" }).ToString(),
-        null => string.Empty,
-        _ => appDetails.ToString(),
-    };
-
-public record AppDetails(string Name, string? Version)
-{
-    public override string ToString()
-        => $"{(string.IsNullOrWhiteSpace(Version) ? "?" : Version)} ({Name})";
-}
+public record Person(string Name, DateOnly DateOfBirth);
